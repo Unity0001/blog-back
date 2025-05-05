bfc2ace73c67405ada34bff578e67094cd99885f
Unity <guilhermtrout@gmail.com>
2025-05-05 17:52:35 -0300

    Update dos models

--- src/models/tag_model.ts
+++ src/models/tag_model.ts
@@ -1,16 +1,71 @@
-import mongoose, { Schema, Document } from "mongoose";
+import slugify from "slugify";
+import { PrismaClient } from "../generated/prisma";
 
-export interface PostDocument extends Document {
-    _id: mongoose.Types.ObjectId;
-    nome: String;
-    slug: String;
-    postTag: mongoose.Types.ObjectId;
+const prisma = new PrismaClient();
+
+export const TagModel = {
+    async createTag(data: {
+        nome: string;
+        slug?: string;
+    }) {
+        const nome = data.nome.trim();
+        const slug = data.slug ?? slugify(data.nome, { lower: true, strict: true })
+
+        return await prisma.tag.create({
+            data: {
+                nome,
+                slug,
+            },
+        });
+    },
+
+    async getTagById(id: string) {
+        return await prisma.tag.findUnique({
+            where: { id },
+            include: {
+                PostTag: true,
+            },
+        });
+    },
+
+    async getTagBySlug(slug: string) {
+        return await prisma.tag.findUnique({
+            where: { slug },
+            include: {
+                PostTag: true,
+            },
+        });
+    },
+
+    async getAllTags() {
+        return await prisma.tag.findMany({
+            include: {
+                PostTag: true,
+            },
+        });
+    },
+
+    async updateTag(id: string, data: { nome?: string; slug?: string }) {
+        const updateData: any = {};
+
+        if (data.nome) {
+            updateData.nome = data.nome.trim();
+            updateData.slug = slugify(data.nome, { lower: true, strict: true })
+        }
+
+        if (data.slug) {
+            updateData.slug = data.slug;
+        }
+
+        return await prisma.tag.update({
+            where: { id },
+            data: updateData,
+        });
+    },
+
+    async deleteTag(id: string) {
+        return await prisma.tag.delete({
+            where: { id },
+        });
+    }
 }
-
-const PostSchema: Schema = new Schema({
-    nome: { types: String, required: true },
-    slug: { types: String, required: true },
-    postTag: { types: Schema.Types.ObjectId, required: true }
-}, { timestamps: true });
-
-export const Post = mongoose.model<PostDocument>('Post', PostSchema);
